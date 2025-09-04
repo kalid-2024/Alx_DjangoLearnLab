bf2c241932270471c7d3187ee4bd07dfbdab7914
kalid-2024 <kalidmohammed236@gmail.com>
2025-09-04 04:20:25 +0300

    feat(bookshelf): create Book model and migrations

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_year = models.IntegerField()
+    published_year = models.IntegerField(default=0)
    
     def __str__(self):
         return self.title
