0a1204b4e231ca8f9f618e25d21ca868bbd026be
kalid-2024 <kalidmohammed236@gmail.com>
2025-09-07 22:05:15 +0300

    included relationship_app/list_books.htmlinside of relationshi_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
 
 # Function-Based View
 def list_all_books(request):
     books = Book.objects.all()
-    lines = [f"{book.title} by {book.author.name}" for book in books]
-    # Join lines into a simple text response
-    return HttpResponse("<br>".join(lines))
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+# Class-based view:
+class LibraryDetailView(DetailView): 
+    model = Library
+    template_name = "relationship_app/library_detail.html"  # Create this template
+    context_object_name = "library"
+
+    # Add all books in the library to context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
