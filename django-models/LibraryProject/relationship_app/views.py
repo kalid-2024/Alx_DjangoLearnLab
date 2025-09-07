0ad849752d0dfd76c3c87b168583b979ef1242fa
kalid-2024 <kalidmohammed236@gmail.com>
2025-09-07 21:55:28 +0300

    created class-based views using django's detailview and function-based view lists all books stored in the database also Edit relationship_app/urls.py to include URL patterns that route to the newly created views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
 
 # Create your views here.
+
+# Function-Based View
+def list_all_books(request):
+    books = Book.objects.all()
+    lines = [f"{book.title} by {book.author.name}" for book in books]
+    # Join lines into a simple text response
+    return HttpResponse("<br>".join(lines))
