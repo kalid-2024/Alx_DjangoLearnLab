454ffc3b477b969ba8fc6dc6fad1ab9ff1ad4319
kalid-2024 <kalidmohammed236@gmail.com>
2025-09-07 21:19:17 +0300

    Included a magic methods which returns self.name

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,14 +4,26 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  # One-to-Many relationship
+
+    def __str__(self):
+        return self.title
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')  # Many-to-Many relationship
 
+    def __str__(self):
+        return self.name
+    
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)  # One-to-One relationship
+
+    def __str__(self):
+        return self.name
