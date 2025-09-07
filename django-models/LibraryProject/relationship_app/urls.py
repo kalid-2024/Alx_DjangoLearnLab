3bcb7c424429b9c5abc6349ce1660f2bbd2fb78c
kalid-2024 <kalidmohammed236@gmail.com>
2025-09-07 22:30:17 +0300

    added login.html, logout.html, register.html templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
 
     # Class-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+     # Authentication
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
 ]
