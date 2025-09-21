42686df028cc1c19425df6bf023868d380f1fb68
kalid-2024 <kalidmohammed236@gmail.com>
2025-09-21 23:03:15 +0300

    Add JWT token blacklist functionality and update views
    - Introduced migrations for token blacklist models and fields.
    - Added models for OutstandingToken and BlacklistedToken.
    - Implemented token management in views, including obtaining and refreshing tokens.
    - Updated BookViewSet permissions to restrict delete actions to admin users.
    - Configured URL patterns for token authentication endpoints.
    - Enhanced token handling with BigAutoField for better scalability.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from .serializers import BookSerializer
 from rest_framework import generics, viewsets
 from .models import Book
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action == 'destroy':  
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
