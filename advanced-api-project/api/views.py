f1deac3d6dda32ecbc73dc60753d9c3b370a62ed
kalid-2024 <kalidmohammed236@gmail.com>
2025-10-02 18:14:15 +0300

    Add missing import statements for DjangoFilterBackend in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class BookListView(generics.ListAPIView):
