52512de75554f24e0eecb3638685ddaed163e6bc
kalid-2024 <kalidmohammed236@gmail.com>
2025-10-04 22:09:04 +0300

    Add initial migration for Blog model and rename to Post; update settings and Docker configuration

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 # Create your models here.
+class Post(models.Model):
+    tittle = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=CASCADE, related_name="posts")
+    def __str__(self):
+        return f"{self.title} ({self.author})"
+   
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "rest_framework",
+    "blog",
+    ""
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
