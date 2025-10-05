46745692a1e06a424ed6b87cdd3cbbd34d7c3ab8
kalid-2024 <kalidmohammed236@gmail.com>
2025-10-05 17:07:48 +0300

    fix(urls): add tag URL pattern and PostByTagListView route

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 #  SEARCH AND TAGGING END POINTS  
     path('search/', views.post_search, name='post_search'),
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='post_by_tag'),
 ]
