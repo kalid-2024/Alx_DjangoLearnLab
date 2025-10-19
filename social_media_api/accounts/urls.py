69eaafc227f95548e5358fc594f41cb15431f364
khalid_mohammed <kalidmohammed236@gmail.com>
2025-10-20 00:06:46 +0300

    fixed ["unfollow/<int:user_id>/"

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:user_id/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
